be1f98f1ac888f112aa664126224f28e3bce554b
Schmidt-avanti <aa.schmidt@ja-dialog.de>
2025-08-09 14:55:31 +0200

    Update TendersApp.swift

--- TendersApp.swift
+++ TendersApp.swift
@@ -1,22 +1,19 @@
 import SwiftUI
 import UserNotifications
 
-/// App-Entry für die TendersApp.
-/// Injiziert die benötigten EnvironmentObjects und fragt Notification-Rechte an.
+/// App-Entry ohne Favoriten-Feature (vorerst deaktiviert)
 @main
 struct TendersAppV2: App {
-    @StateObject private var favs = FavoritesManager.shared
     @StateObject private var savedSearches = SavedSearchManager.shared
     @StateObject private var bidManager = BidManager.shared
 
     var body: some Scene {
         WindowGroup {
             RootView()
-                .environmentObject(favs)
                 .environmentObject(savedSearches)
                 .environmentObject(bidManager)
                 .onAppear {
-                    // Benachrichtigungsberechtigung einmalig anfragen
+                    // Benachrichtigungsrechte anfragen (einmalig)
                     NotificationManager.shared.requestPermission()
                 }
         }
