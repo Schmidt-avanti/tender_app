28effc8834e155600ec599a7f09c37ec6cb12bb3
Schmidt-avanti <aa.schmidt@ja-dialog.de>
2025-08-09 17:02:01 +0200

    Update TendersApp.swift

--- TendersApp.swift
+++ TendersApp.swift
@@ -2,11 +2,33 @@ import SwiftUI
 
 @main
 struct TendersApp: App {
+    // Alle benötigten Models einmalig erzeugen und in den View-Tree geben
+    @StateObject private var searchVM = SearchViewModel()
+    @StateObject private var bidManager = BidManager()
+    @StateObject private var savedSearchManager = SavedSearchManager()
+
+    init() {
+        // Optional: Navigationsleiste transparent halten (wir malen selbst Hintergrund)
+        let nav = UINavigationBarAppearance()
+        nav.configureWithTransparentBackground()
+        UINavigationBar.appearance().standardAppearance = nav
+        UINavigationBar.appearance().scrollEdgeAppearance = nav
+    }
+
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .background(Color(.systemBackground))   // Hintergrund wie System
-                .ignoresSafeArea()                       // bis in die Statusbar malen
+            ZStack {
+                // Vollflächiger Hintergrund (unter Safe Areas)
+                Color(uiColor: .systemBackground)
+                    .ignoresSafeArea()
+
+                // Deine eigentliche App
+                RootView()
+            }
+            // EnvironmentObjects bereitstellen (sonst Crash)
+            .environmentObject(searchVM)
+            .environmentObject(bidManager)
+            .environmentObject(savedSearchManager)
         }
     }
 }
