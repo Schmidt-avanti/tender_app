09013fb1fa36d41ce0595e3537888209ca866818
Schmidt-avanti <aa.schmidt@ja-dialog.de>
2025-08-09 10:31:46 +0200

    Update Filters.swift

--- Models/Filters.swift
+++ Models/Filters.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-/// Defines the search filters used to query the backend.  All fields
-/// are optional, allowing the user to specify as much or as little
-/// constraint as desired.
 struct SearchFilters: Codable, Equatable, Hashable {
     var cpv: [String] = []
     var regions: [String] = []
