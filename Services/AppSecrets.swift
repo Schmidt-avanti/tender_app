db3f0596667c833a0f48c67fccb7860865eb1b34
Schmidt-avanti <aa.schmidt@ja-dialog.de>
2025-08-11 09:53:46 +0200

    Delete Services directory

--- Services/AppSecrets.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-enum AppSecrets {
-    static let openAIKey  = ProcessInfo.processInfo.environment["OPENAI_API_KEY"] ?? ""
-    static let serpApiKey = ProcessInfo.processInfo.environment["SERP_API_KEY"] ?? ""
-    static let bingApiKey = ProcessInfo.processInfo.environment["BING_API_KEY"] ?? ""
-}
